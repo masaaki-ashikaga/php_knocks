a8d0db9595e691beab8182c00bc1f8de78d2c7c1
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-04-22 22:04:22 +0900

    1本目_CopyRight

---
+++ one_five/one.php
@@ -0,0 +1,15 @@
+<?php
+$year = date('Y');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>自動更新CopyRight</title>
+</head>
+<body>
+  <?php
+    echo '©️ CopyRight ' . $year . ' 脳科学 × 動画 Marketing All Rights Reserved.';
+  ?>
+</body>
+</html>
